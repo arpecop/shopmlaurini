e435e61e0720063f921947977da5d51b1d97164f
Rudi <rudix.lab@gmail.com>
2021-10-06 12:49:47 +0300

    dd

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,14 +4,16 @@ import Navbar from './Navbar'
 
 const Layout = ({ children, categories }) => {
   return (
-    <div className='flex justify-center bg-gray-200'>
-      <div className='max-w-screen-lg flex flex-col min-h-screen w-full'>
-        <Navbar />
-        <CategoryButtons categories={categories} />
-        <div className='flex-grow'>{children}</div>
-        <Footer />
+    <>
+      <Navbar />
+      <div className='flex justify-center bg-gray-200'>
+        <div className='max-w-screen-lg flex flex-col min-h-screen w-full'>
+          <CategoryButtons categories={categories} />
+          <div className='flex-grow'>{children}</div>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
